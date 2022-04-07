d1c08c7c9c4969dcb4f4223f811e3d3247c333c7
yash91989201 <yashraj6370@gmail.com>
2022-04-07 22:39:51 +0530

    fix : add signout url

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -33,6 +33,12 @@ export default NextAuth({
       }
       return session;
     },
+    redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) return url;
+      // Allows relative callback URLs
+      else if (url.startsWith("/")) return new URL(url, baseUrl).toString();
+      return baseUrl;
+    },
   },
   secret: process.env.SECRET,
   jwt: {
